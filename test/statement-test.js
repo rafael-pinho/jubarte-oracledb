27cf82f7c5035fc57ee2e2c6f26ddf98cd8a2f85
Rafael <rp.rafaelpinho@gmail.com>
2016-08-28 13:12:14 -0300

    new tests

---
+++ test/statement-test.js
@@ -0,0 +1,70 @@
+let assert = require('assert'),
+    statement = require('../lib/statement/statement.js');
+
+describe('statement buider', function() {
+    it('should have a sql command', function(done) {
+        let command = 'SELECT SYSDATE FROM DUAL',
+            fake = statement.create(command);
+        assert(fake);
+        assert.equal(fake.command, command);
+        done(null);
+    });
+
+    it('should get sysdate from oracle', function(done) {
+        this.timeout(5000);
+        let command = 'SELECT SYSDATE FROM DUAL',
+            fake = statement.create(command).execute({
+                user: process.env.ORACLE_USER, 
+                password: process.env.ORACLE_PASSWORD, 
+                connectString: process.env.ORACLE_CONNECTION_STRING 
+            }, (err, data) => {
+                assert.equal(err, null);
+                assert.ok(data.rows[0][0]);
+                done(null);
+            });
+    });
+
+    it('should execute a sql command and send a parameter array', function(done) {
+        let p1 = 'Noe',
+            p2 = 45
+            command = 'SELECT :name, :age FROM DUAL',
+
+            fake = statement
+                        .create(command)
+                        .addParameters(p1, p2)
+                        .execute({
+                            user: process.env.ORACLE_USER, 
+                            password: process.env.ORACLE_PASSWORD, 
+                            connectString: process.env.ORACLE_CONNECTION_STRING 
+                        }, (err, data) => {
+                            assert.equal(err, null);
+                            assert.ok(data.rows[0]);
+                            assert.equal(data.rows[0][0], p1);
+                            assert.equal(data.rows[0][1], p2);
+                            done(null);
+                        });
+    });
+
+    it('should execute a sql command and send parameters as objects', function(done) {
+        let p1 = 'Johan',
+            p2 = 45
+            command = 'SELECT :name, :age FROM DUAL',
+
+            fake = statement
+                        .create(command)
+                        .addParameters()
+                            .name('name').value(p1)
+                            .name('age').value(p2)
+                        .execute({
+                            user: process.env.ORACLE_USER, 
+                            password: process.env.ORACLE_PASSWORD, 
+                            connectString: process.env.ORACLE_CONNECTION_STRING 
+                        }, (err, data) => {
+                            assert.equal(err, null);
+                            assert.ok(data.rows[0]);
+                            assert.equal(data.rows[0][0], p1);
+                            assert.equal(data.rows[0][1], p2);
+                            done(null);
+                        });
+    });
+});
